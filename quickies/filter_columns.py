9dfe2fe5ef22a37ea68f2b753b3d4f660e5b7f74
harsilspatel <harsilspatel@gmail.com>
2019-08-31 16:38:28 +1000

    update column filtering script to process reliability columns

--- quickies/filter_columns.py
+++ quickies/filter_columns.py
@@ -1,3 +1,4 @@
+import csv
 import sys
 import pandas as pd
 
@@ -8,20 +9,33 @@ columns_to_keep = ['SAMPLING_METHOD_DESC',
                    'LONGITUDEDD_NUM', 'LAT_LONG_ACCURACYDD_INT', 'TOTAL_COUNT_INT']
 
 def is_reliable(RELIABILITY, RATING_INT):
-	# print(RELIABILITY, RATING_INT)
 	return not(RELIABILITY in ('', 'Unconfirmed') and RATING_INT in ('2', '4'))
 
-
+# Latitude, Longit
 def main():
 	filename = sys.argv[1]
 
-	f = pd.read_csv(filename)
-	new_f = f[columns_to_keep]
+	new_file = filename[:-4] + '_filtered.csv'
 
-	for index, row in new_f.iterrows():
-		print(index)
-		print(row)
-	new_f.to_csv(filename[:-4] + '_filtered.csv', index=False)
+	with open(filename, 'r') as source:
+		rdr = csv.DictReader(source)
+		with open(new_file, 'w') as result:
+			wtr = csv.writer(result)
+			for r in rdr:
+				reliability = is_reliable(r['RELIABILITY'], r['RATING_INT'])
+				wtr.writerow((reliability, r['SURVEY_START_DATE'], r['LATITUDEDD_NUM'], r['LONGITUDEDD_NUM'], r['LAT_LONG_ACCURACYDD_INT'], r['TOTAL_COUNT_INT']))
+
+	with open(new_file, 'r') as original: data = original.read()
+	with open(new_file, 'w') as modified: modified.write('is_reliable,survey_date,latitude,longitude,lat_lng_accuracy,total_count\n' + data)
+
+
+# f = pd.read_csv(filename)
+	# new_f = f[columns_to_keep]
+	#
+	# for index, row in new_f.iterrows():
+	# 	print(index)
+	# 	print(row)
+	# new_f.to_csv(, index=False)
 
 
 if __name__ == '__main__':
