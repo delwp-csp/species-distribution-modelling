2aaace509b39fbb52a6ef48fa96f1b9334bc8f88
Luke Silva <github@lukesilva.com>
2019-09-21 19:08:13 +1000

    Run the data through a simple model

--- ai/README.md
+++ ai/README.md
@@ -3,7 +3,7 @@
 First install miniconda / anaconda
 Run the following to install the packages
 ```
-conda create -n gdal -c conda-forge gdal tiledb=1.6.0
+conda create -n gdal -c conda-forge gdal tiledb=1.6.0 scikit-learn imbalanced-learn matplotlib matplotlib-base mpl_sample_data pandas
 ```
 
 Then whenever you want to run gdal, use `conda activate gdal`
--- ai/main.py
+++ ai/main.py
@@ -3,12 +3,17 @@ import sys
 import pandas as pd
 import numpy as np
 from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN
-
+from sklearn.preprocessing import scale
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 from collections import Counter
 
+from transform import add_vic_coordinates
 from filter_columns import filter_columns
 from balance import balance
-
+from vector import ProcessPoints
+from add_env_data import add_columns
 
 def main(fname):
     dataset = pd.read_csv(fname)#.sort_values(by='is_reliable')
@@ -19,6 +24,38 @@ def main(fname):
     print("smote", Counter(balanced_data['smote'][1]))
     print("adasyn", Counter(balanced_data['adasyn'][1]))
 
+    x, y = balanced_data['smote']
+    x = add_vic_coordinates(x)
+    print(x)
+    x = add_columns(x)
+    #x[['latitude', 'longitude']] = scale(x[['latitude', 'longitude']])
+    print(x)
+    scaled = pd.DataFrame(scale(x), index=x.index, columns=x.columns)
+    scaled['vic_x'] = x['vic_x']
+    scaled['vic_y'] = x['vic_y']
+    x = scaled
+    print(x)
+    
+
+    for f in ['../raw_dataset/VMLITE/VMLITE_BUILT_UP_AREA.shp', '../raw_dataset/VMLITE/VMLITE_FOREST_SU2.shp',
+        '../raw_dataset/VMLITE/VMLITE_HY_WATER_AREA.shp', '../raw_dataset/VMLITE/VMLITE_PUBLIC_LAND_SU3.shp']:
+        print(f)
+        x = ProcessPoints(x, f)
+
+    print(x)
+    x = x.drop(columns=['vic_x', 'vic_y'])
+    print(x)
+    
+    
+    xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size = 0.1)
+
+    md = svm.SVC(gamma='scale')
+    md.fit(xTrain, yTrain)
+
+    predictions = md.predict(xTest)
+
+    accuracy = accuracy_score(yTest, predictions)
+    print(accuracy)
 
 
 
