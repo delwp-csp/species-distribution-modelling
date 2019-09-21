45878317755bdd2efdaf34fc38f6ad8c39dbb2d1
harsilspatel <harsilspatel@gmail.com>
2019-09-21 18:28:00 +1000

    add raster data
    omg this pandas is just so coooooollll <3<3  wo yao ni de aiiiii

--- ai/add_env_data.py
+++ ai/add_env_data.py
@@ -4,20 +4,28 @@ import vector
 import transform
 
 try:
-    import pandas as pd
-    import numpy as np
+	import pandas as pd
+	import numpy as np
 except:
-    print("Failed to load packages, make sure your conda environment is setup correctly")
-    sys.exit(1)
+	print("Failed to load packages, make sure your conda environment is setup correctly")
+	sys.exit(1)
 
 
-def get_vic_coordinates(row):
-    return pd.Series(transform.latlong2vicmap((row['latitude'], row['longitude'])))
+def add_raster_data(data, fname, column_name):
+	rp = raster.RasterProcessor(fname)
+	data[column_name] = data.apply(lambda row: rp.GetValueAt(row['vic_x'], row['vic_y']), axis=1)
+	return data
 
-def get_env_data(data):
-    data[['vic_x', 'vic_y']] = data.apply(get_vic_coordinates)
+
+def add_columns(data):
+	for raster_file, column in [('../raw_dataset/sept2014JanMaxTemp/sept2014JanMaxTemp.ers', 'jan_max_temp'),
+	                            ('../raw_dataset/sept2014JanRainfall/sept2014JanRainfall.ers', 'jan_rainfall'),
+	                             ('../raw_dataset/sept2014JulMinTemp/sept2014JulMinTemp.ers', 'jul_min_temp'),
+	                              ('../raw_dataset/sept2014JulRainfall/sept2014JulRainfall.ers', 'jul_rainfall')]:
+		data = add_raster_data(data, raster_file, column)
+
+	return data
 
 
 if __name__ == '__main__':
-    data = pd.read_csv('../dataset/Agile_Antechinus.csv')
-
+	data = pd.read_csv('../dataset/Agile_Antechinus.csv')
