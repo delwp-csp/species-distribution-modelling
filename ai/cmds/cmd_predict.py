d99c1320e7921d8ac6e236b6dc6eadab52b90aa1
harsilspatel <harsilspatel@gmail.com>
2019-10-04 13:45:09 +1000

    add predictions command

---
+++ ai/cmds/cmd_predict.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from joblib import load
+
+
+def predict(args):
+	dataset = pd.read_csv(args.infile)
+	x, y = dataset[args.training_type]
+
+	model = load(args.modelfile)
+	predictions = model.predict(x)
+
+	if args.outfile:
+		pd.DataFrame(predictions, columns=['predictions']).to_csv(args.outfile)
+
+	return predictions
