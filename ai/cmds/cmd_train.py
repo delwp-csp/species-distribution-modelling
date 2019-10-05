bedce57cde424559a2ecd960dc7171a08e8fad90
harsilspatel <harsilspatel@gmail.com>
2019-10-05 10:41:59 +1000

    refactored train to use the train_model() from library

--- ai/cmds/cmd_train.py
+++ ai/cmds/cmd_train.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from joblib import dump
+from lib.train import train_model
+
 
 
 
@@ -10,10 +12,7 @@ def train(args):
 		print("Warning: Training data without saving the model is useless...")
 
 	dataset = pd.read_csv(args.infile)
-	x, y = dataset[args.training_type]
-
-	model = svm.SVC(gamma='scale')
-	model.fit(x, y)
+	model = train_model(dataset, args.training_type)
 
 	if args.modelfile:
 		dump(model, args.modelfile)
