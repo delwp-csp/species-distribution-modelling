e1a0e9fdb93b607c636e68e67bc201f015bcfc78
Luke Silva <github@lukesilva.com>
2019-10-06 15:45:12 +1100

    Adjust columns to speed up & see messages in realtime

--- ai/lib/add_env_data.py
+++ ai/lib/add_env_data.py
@@ -26,14 +26,13 @@ def add_columns(data):
         "../raw_dataset/75m_dem_streams_burned_sept2012/75m_dem_streams_burned_sept2012",
         "../raw_dataset/ProtectionIndex/ProtectionIndex",
         "../raw_dataset/Radiometrics_2014_th/Radiometrics_2014_th",
-        "../raw_dataset/SummerLandsat75_300_900m-005/SummerLandsat75_300_900m-005",
-        "../raw_dataset/SummerPre1750Landsat75_300_900m-003/SummerPre1750Landsat75_300_900m-003",
+        "../raw_dataset/SummerLandsat75_300_900m-005/SummerLandsat75_300_900m",
+        "../raw_dataset/SummerPre1750Landsat75_300_900m-003/SummerPre1750Landsat75_300_900m",
         "../raw_dataset/ecoregion1750/ecoregion1750",
         "../raw_dataset/ecoregion2014/ecoregion2014",
-        "../raw_dataset/hydro500xwi-004/hydro500xwi-004",
+        "../raw_dataset/hydro500xwi-004/hydro500xwi",
         "../raw_dataset/ibra_hex/ibra_hex",
         "../raw_dataset/land_cov_use3/land_cov_use3",
-        "../raw_dataset/rasterfiles/rasterfiles",
         "../raw_dataset/sept2014JanMaxTemp/sept2014JanMaxTemp",
         "../raw_dataset/sept2014JanRainfall/sept2014JanRainfall",
         "../raw_dataset/sept2014JulRainfall/sept2014JulRainfall",
@@ -41,7 +40,7 @@ def add_columns(data):
         "../raw_dataset/wetness_index_saga_sept2012/wetness_index_saga_sept2012"
     ]:
         if os.path.exists(raster_file):
-            print("Processing: {}".format(raster_file))
+            print("Processing: {}".format(raster_file), flush=True)
             data = add_raster_data(data, raster_file)
         else:
             print("Warning: skipping file {} since it doesn't exist".format(raster_file))
@@ -49,15 +48,15 @@ def add_columns(data):
     for vector_file in [
         '../raw_dataset/VMLITE/VMLITE_BUILT_UP_AREA.shp',
         '../raw_dataset/VMLITE/VMLITE_FOREST_SU2.shp',
-        '../raw_dataset/VMLITE/VMLITE_FOREST_SU3.shp',
-        '../raw_dataset/VMLITE/VMLITE_FOREST_SU5.shp',
-        '../raw_dataset/VMLITE/VMLITE_HY_WATER_AREA.shp',
-        '../raw_dataset/VMLITE/VMLITE_HY_WATERCOURSE.shp',
+        #'../raw_dataset/VMLITE/VMLITE_FOREST_SU3.shp',
+        #'../raw_dataset/VMLITE/VMLITE_FOREST_SU5.shp',
+        #'../raw_dataset/VMLITE/VMLITE_HY_WATER_AREA.shp',
+        #'../raw_dataset/VMLITE/VMLITE_HY_WATERCOURSE.shp',
         '../raw_dataset/VMLITE/VMLITE_PUBLIC_LAND_SU3.shp',
-        '../raw_dataset/VMLITE/VMLITE_PUBLIC_LAND_SU5.shp'
+        #'../raw_dataset/VMLITE/VMLITE_PUBLIC_LAND_SU5.shp'
     ]:
         if os.path.exists(vector_file):
-            print("Processing: {}".format(vector_file))
+            print("Processing: {}".format(vector_file), flush=True)
             data = vector.ProcessPoints(data, vector_file)
         else:
             print("Warning: skipping file {} since it doesn't exist".format(raster_file))
