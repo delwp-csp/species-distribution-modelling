e673917deed816199e7b96a0a1262aeda0110feb
Luke Silva <github@lukesilva.com>
2019-10-19 18:52:16 +1100

    Add test for Button

---
+++ frontend/src/components/button.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import Button from './button'
+
+
+it('Clicking on a button calls onClick', () => {
+    const spy = jest.fn()
+    const {getByText} = render(<Button buttonText="Test Button" onClick={spy}/>)
+
+    fireEvent.click(getByText('Test Button'))
+
+    expect(spy).toBeCalled()
+})
